fa707e81b3315ed27f37ceeee8d487814abe3979
ckozak <ckozak@redhat.com>
2013-05-07 17:18:45 -0400

    no comment

--- coffeetimethreading.py
+++ coffeetimethreading.py
@@ -53,13 +53,13 @@ class CoffeeMakerSingleton:
 
     def end_timers(self):
         print 'threads'
-        '''for t in threading.enumerate():
+        for t in threading.enumerate():
             print t
             if isinstance(t, threading._Timer):
                 print 'canceling'
                 t.cancel()
                 print 'joining'
-                t.join()'''
+                t.join()
 
     def force_stop(self):
         self.end_timers()
