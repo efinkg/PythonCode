72233c0b89d0812960eaf9b5389a83a12820b9fb
Ethan <efinkg@gmail.com>
2013-05-08 13:14:38 -0400

    font size

--- MakeCoffee.py
+++ MakeCoffee.py
@@ -33,9 +33,11 @@ html = """
 </script>
 </head>
 <body>
+<font size = 16>
     <input id="coffeeoz" type="number" name="quantity" min="12" max="100" value="16"> Oz of coffee<br>
     <input id="coffeebutton" value="Start Coffee" type="submit">
     <input id="stopbutton" value="Stop Coffee" type="submit">
+<font>
 </body>
 </html>
 """
